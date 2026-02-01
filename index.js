3516ad6f987be29619e14c5ec83cc7fd5e2e13eb
Techshubham45 <149092799+Shubham45264@users.noreply.github.com>
2026-02-01 13:36:25 +0530

    Add backend health check route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Sai Associates Backend is running...");
+});
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
